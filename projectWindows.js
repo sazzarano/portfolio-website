c5eae37526683e3e303d5c9e47a6e64ed0de69fe
sazzarano <sam.azzarano@gmail.com>
2019-11-10 22:35:43 -0500

    Popup windows under construction...

---
+++ projectWindows.js
@@ -0,0 +1,22 @@
+const popupWindows = () => {
+
+}
+
+popupWindows();
+
+/* 
+PSEUDO CODE HERE
+
+allButtons stores all buttons by class, 
+added event listener for a click, 
+codeWindow will be a variable assignment of pop-up code windows.
+*/
+
+/* 
+HOW IT'S SUPPOSED TO WORK
+
+Each button opens a different project window which displays the html, css 
+and js of the project within tab buttons. This can be done via a pdf file or 
+with another method unknown to me. This window can also be closed via the 
+close button on it's corner.
+*/
