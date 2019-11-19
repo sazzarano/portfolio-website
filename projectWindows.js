d251bb2c54eacb161022dd54baef17b9ac3f7f5e
sazzarano <sam.azzarano@gmail.com>
2019-11-18 22:37:44 -0500

    Many changes-- including code button rethink and streamlining for changes down the road.

--- projectWindows.js
+++ projectWindows.js
@@ -12,18 +12,6 @@ closeButton.onclick = () => {
     popupWindow.style.display = "none";
 }
 
-
-
 /* 
-PSEUDO CODE HERE
-
-*/
-
-/* 
-HOW IT'S SUPPOSED TO WORK
-
-Each button opens a different project window which displays the html, css 
-and js of the project within tab buttons. This can be done via a pdf file or 
-with another method unknown to me. This window can also be closed via the 
-close button on it's corner.
+This script is no longer necessary. It is here for archival purposes or reuse only.
 */
